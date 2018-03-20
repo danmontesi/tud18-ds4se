fcf3e0c6b85f4cebae622838fac2d14cfe052bb5
Michael Schwarz <michael.schwarz93@gmail.com>
2018-03-20 11:38:55 +0100

    traverse commits to build index

--- analysis.py
+++ analysis.py
@@ -1,32 +1,37 @@
 from git import Repo, Tree, Blob, Submodule
 
 
+
 def run():
     # open repository
     repo = Repo("../rust")
-    handle_commit(repo.tag("refs/tags/1.14.0").commit)
-    count = 0
+
+    results = {}
+    visited = {}
+
+    to_visit = [repo.tag("refs/tags/1.14.0").commit]
+    while len(to_visit) > 0:
+        handle_commit(to_visit.pop(), results, visited, to_visit)
+    print(results)
 
 
-
-def handle_commit(commit):
+def handle_commit(commit, results, visited, to_visit):
     blobs = 0
 
-    # for i in commit.tree.traverse():
-    for i in commit.:
-        if isinstance(i, Tree):
-            print("tree")
-            # ignore for now
-        elif isinstance(i, Submodule):
-            # we are not considering submoules to be part of this software product
-            pass
-        else:
-            print(i.path)
-            print(type(i))
-            assert isinstance(i, Blob)
-            blobs += 1
+    visited[commit.binsha] = True
 
-    print(blobs)
+    for parent in commit.parents:
+        for x in commit.diff(parent):
+            if x.b_path not in results:
+                results[x.b_path] = {}
+
+            if commit.author not in results[x.b_path]:
+                results[x.b_path][commit.author] = 0
+
+            results[x.b_path][commit.author] += 1
+
+        if parent.binsha not in visited:
+            to_visit.append(parent)
 
 if __name__ == "__main__":
     run()
