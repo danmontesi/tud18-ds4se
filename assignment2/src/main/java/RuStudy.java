dd5741f0796f4458135020152ae1f214f9e1f204
danmontesi <daniele.montesi@mail.polimi.it>
2018-03-21 10:29:42 +0100

    added assignment2

---
+++ assignment2/src/main/java/RuStudy.java
@@ -0,0 +1,16 @@
+import org.repodriller.RepoDriller;
+import org.repodriller.Study;
+
+public class RuStudy implements Study {
+
+    public static void main(String[] args) {
+        new RepoDriller().start(new RuStudy());
+    }
+
+
+    public void execute() {
+
+
+    }
+
+}
