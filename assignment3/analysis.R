88543988a9023927559b3bc36db0c7c5dc2d0be6
danmontesi <daniele.montesi@mail.polimi.it>
2018-03-22 10:08:21 +0100

    Refactoring

--- assignment3/analysis.R
+++ assignment3/analysis.R
@@ -1,11 +1,14 @@
-setwd("e:\\tud18\\tud18-ds4se\\assignment3\\")
-ds <- read.csv("..\\assignment2\\results.tsv",header = TRUE, sep = "\t", quote = "",na.strings = "null")
+setwd("/Users/danmontesi/Desktop/tud18-ds4se/assignment3")
+ds <- read.csv("../assignment2/results.tsv",header = TRUE, sep = "\t", quote = "",na.strings = "null")
 control_vars <- list("churn", "size")
 independent_vars <- list("minor", "major", "total", "ownership")
 dependent_var <- "defects"
+
+#Filter file extension ####
 filenames <- as.vector(ds$filename)
+#install.packages("stringr")
 
 library(stringr)
-test <- str_match(filenames, ".*(\\.\\w*)$")
-table(test[,2])
+extension <- str_match(filenames, ".*(\\.\\w*)$")
+table(extension[,2])
 good_extensions <- list(".c", ".cpp", ".h", ".rs", ".json", ".toml", ".mk")
