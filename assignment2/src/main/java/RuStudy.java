497100b6d7c5724d70291eca23df74a274dd3438
Sebastian <ober.sebastian@googlemail.com>
2018-03-22 11:55:04 +0100

    Changed to gimp

--- assignment2/src/main/java/RuStudy.java
+++ assignment2/src/main/java/RuStudy.java
@@ -11,14 +11,14 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class RuStudy implements Study {
 
-    private static String repoPath = "/u/halle/obers/home_at/temp/rust";
+    private static String repoPath = "/u/halle/obers/home_at/temp2/gimp";
 
-    private static String earliestCommit = "3d7cd77e442ce34eaac8a176ae8be17669498ebc"; // dec 10 2015
-    private static String commitAtRelease = "e8a0123241f0d397d39cd18fcc4e5e7edde22730"; // dec 22 2016
-    private static String commitYearAfterRelease = "766bd11c8a3c019ca53febdcd77b2215379dd67d"; // jan 4 2018
+    private static String earliestCommit = "92f2b0092925a52e8ea9436a2e2745bd32edaa54"; // nov 25 2015
+    private static String commitAtRelease = "91b41bba51af9635b97847ee307e86c220ee0657"; // feb 1 2017
+    private static String commitYearAfterRelease = "18794a6ba2915ed58b82337edaba794d69f767b7"; // dec 12 2017
 
-    private static Calendar noBugsIntroducedBefore =  new GregorianCalendar(2016,11,22);
-    private static Calendar noBugsIntroducedAfter = new GregorianCalendar(2017,5,22);
+    private static Calendar noBugsIntroducedBefore =  new GregorianCalendar(2017,1,1);
+    private static Calendar noBugsIntroducedAfter = new GregorianCalendar(2017,6,13);
 
     public void execute() {
         ConcurrentHashMap<String ,Integer> defectsMap = new ConcurrentHashMap<>();
