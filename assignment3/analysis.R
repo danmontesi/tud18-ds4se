b29fb2c9d61b099ad38269bed5dc80a02ca7c02a
danmontesi <daniele.montesi@mail.polimi.it>
2018-03-22 13:23:15 +0100

    Added more filters

--- assignment3/analysis.R
+++ assignment3/analysis.R
@@ -12,3 +12,37 @@ library(stringr)
 extension <- str_match(filenames, ".*(\\.\\w*)$")
 table(extension[,2])
 good_extensions <- list(".c", ".cpp", ".h", ".rs", ".json", ".toml", ".mk")
+cleaned_filenames <- filenames[extension[,2] %in% good_extensions]
+cleaned_ds <- ds[ds$filename%in%cleaned_filenames, ]
+
+# Filter size ####
+head(cleaned_ds[order(cleaned_ds$size, decreasing = TRUE), ], n = 100)
+cleaned_ds <- cleaned_ds[cleaned_ds$size < 4000, ]
+ 
+# Filter defect count ####
+head(cleaned_ds[order(cleaned_ds$defects, decreasing = TRUE), ], n = 100)
+
+# Add binary features ####
+cleaned_ds$has_defect <- as.integer(cleaned_ds$defects > 0)
+
+# Plot ####
+pairs(cleaned_ds)
+
+# Linear model on control variables
+model <- lm(cleaned_ds$defects ~ cleaned_ds$churn + cleaned_ds$size)
+
+# Linear Model ####
+model <- lm(cleaned_ds$defects ~ cleaned_ds$minor + cleaned_ds$major + cleaned_ds$ownership)
+
+# Linear Model combined ####
+model <- lm(cleaned_ds$defects ~ cleaned_ds$churn + cleaned_ds$size + cleaned_ds$minor + cleaned_ds$major + cleaned_ds$ownership)
+plot(model)
+confint(model)
+coef(model)
+summary(model)
+
+cleaned_ds <- cleaned_ds[cleaned_ds$ownership < 1, ]
+model <- lm(cleaned_ds$defects ~ cleaned_ds$ownership )
+plot(model)
+summary(model)
+pairs(cleaned_ds)
